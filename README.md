4604063dd7aa4439c93699dca8e897252c3268f8
Sheldon <a@a.com>
2024-01-18 10:37:03 -0300

    Dia 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# qa_challenge_python
+# Intensivo QA que programa
+
+21 dias com desafios em python.
+Com [Paulo Oliveira](https://github.com/paulocoliveira/desafio-qa-que-programa)
---
+++ d1.py
@@ -0,0 +1,9 @@
+print('Descobrirei se o seu número é par ou impar!')
+
+numero = int(input('\nDigite um número: '))
+
+
+if numero % 2 == 0:
+    print('É par!')
+else:
+    print('É ímpar!')
